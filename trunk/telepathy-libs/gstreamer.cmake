5e696c4a0cd65018c02bd0055ee765fd69bdc0eb
matti.reijonen@evocativi.com <matti.reijonen@evocativi.com@00382f00-05a3-11df-962c-b5c252e50527>
2010-01-20 14:26:17 +0000

    updating telepathy-blib cmake build to correspond build deps folders and making tp-glib readme-win32.txt up to date

--- trunk/telepathy-libs/gstreamer.cmake
+++ trunk/telepathy-libs/gstreamer.cmake
@@ -3,7 +3,7 @@
 #  GSTREAMER_LIB_DIR       - folder where to find gstreamer.lib
 #  LIBXML2_INCLUDE_DIR     - folder where to find libxml/parser.h
 
-set(GSTREAMER_INCLUDE_DIR "$ENV{NAALI_DEP_PATH}/gstreamer/include/gstreamer-0.10")
+set(GSTREAMER_INCLUDE_DIR "$ENV{NAALI_DEP_PATH}/gstreamer/include")
 set(GSTREAMER_LIB_DIR "$ENV{NAALI_DEP_PATH}/gstreamer/lib")
 set(LIBXML2_INCLUDE_DIR "C:/gstreamer/include/libxml2")
 

47c513ba1f1b770a0b9aa0945db1df5387652fd7
matti.reijonen@evocativi.com <matti.reijonen@evocativi.com@00382f00-05a3-11df-962c-b5c252e50527>
2010-01-22 11:39:28 +0000

    Still need these Contact::Feature's for showing presence statuses

--- trunk/telepathy-libs/telepathy-qt4/TelepathyQt4/channel.cpp
+++ trunk/telepathy-libs/telepathy-qt4/TelepathyQt4/channel.cpp
@@ -663,7 +663,11 @@ void Channel::Private::buildContacts()
     }
 
     PendingContacts *pendingContacts = manager->contactsForHandles(
-            toBuild);
+            toBuild,
+            QSet<Contact::Feature>() << Contact::FeatureAlias
+                                     << Contact::FeatureAvatarToken
+                                     << Contact::FeatureSimplePresence);
+
     parent->connect(pendingContacts,
             SIGNAL(finished(Tp::PendingOperation *)),
             SLOT(gotContacts(Tp::PendingOperation *)));

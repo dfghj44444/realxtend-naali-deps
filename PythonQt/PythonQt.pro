b68f79e03f917770149923a1a124d25d13e9c6e4
toni.alatalo <toni.alatalo@00382f00-05a3-11df-962c-b5c252e50527>
2010-05-25 19:08:36 +0000

    Remove old self patched PythonQt. The patch we needed and good improvements have made it to upstream, we now use the current 2.0.1 version from there unmodified.

--- PythonQt/PythonQt.pro
+++
@@ -1,5 +0,0 @@
-TEMPLATE = subdirs
-
-CONFIG += ordered
-SUBDIRS = src extensions
-#SUBDIRS = generator src extensions tests examples
--- PythonQt/generated_cpp/com_trolltech_qt_core/PythonQtWrapper_Qt.cpp
+++
@@ -1,8 +0,0 @@
-#include "PythonQtWrapper_Qt.h"
-
-#include <PythonQtSignalReceiver.h>
-#include <PythonQtMethodInfo.h>
-#include <PythonQtConversion.h>
-#include <QTextDocument>
-#include <QVariant>
-
